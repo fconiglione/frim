167f6eb923334adf89eda9ca3c21f5ab5b3347f1
fconiglione <122475578+fconiglione@users.noreply.github.com>
2024-02-17 11:17:00 -0500

    fixed file hierarchy

---
+++ src/config/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../assets/styles/normalize.css";
+import "../assets/styles/App.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "../pages/index";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <section className="home-masthead">
+            <h1>Hello, World!</h1>
+        </section>
+    );
+}
+
+export default Home;
