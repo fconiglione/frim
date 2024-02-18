f2534923e6ec6ef7e92d1903cb89985ebfbce348
fconiglione <122475578+fconiglione@users.noreply.github.com>
2024-02-18 16:50:54 -0500

    home-masthead complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import HomeMastheadImg from "../assets/images/home-masthead-img.png";
 
 function Home() {
     const pageTitle = "Foundational Tools for Startups";
@@ -9,7 +10,31 @@ function Home() {
     return (
         <main className="home">
             <section className="home-masthead">
-                <h1>Hello, World!</h1>
+                <div className="home-masthead-column">
+                    <div className="home-masthead-row">
+                        <p className="coming-soon">*Coming Soon*</p>
+                        <h1>Grow faster with solutions designed for startup ventures</h1>
+                    </div>
+                    <div className="home-masthead-row">
+                        <div>
+                            <a className="btn-1" href="/get-started">
+                                <span>Get Started for Free</span>
+                                <i className="fa-solid fa-angles-right"></i>
+                            </a>
+                        </div>
+                        <div>
+                            <a className="btn-2" href="/demo">
+                                <span>Watch Demo</span>
+                                <i className="fa-solid fa-angles-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="home-masthead-column">
+                    <div>
+                        <img src={HomeMastheadImg} alt="A black and white road graphic"/>
+                    </div>
+                </div>
             </section>
         </main>
     );
