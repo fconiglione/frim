5e39fee1d9a175b69f4be97e181182cf9c01b2d4
fconiglione <122475578+fconiglione@users.noreply.github.com>
2024-02-26 15:15:23 -0500

    error page completed

--- src/pages/error.js
+++ src/pages/error.js
@@ -11,11 +11,13 @@ function Error404() {
             <div>
                 <h1>404</h1>
                 <h1>Page Not Found</h1>
+            </div>
+            <div>
                 <p>Sorry, the page you are looking for does not exist.</p>
             </div>
             <div>
                 <a href="/" className="btn-2">
-                    <span>Return Home</span>
+                <span>Return Home</span>
                     <i className="fa-solid fa-angles-right"></i>
                 </a>
             </div>
