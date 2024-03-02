6d898e61192a84d3522bd4590214771217e72528
fconiglione <122475578+fconiglione@users.noreply.github.com>
2024-03-02 12:05:18 -0500

    careers-masthead complete

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from "react";
+import CareersMastheadImage from "../assets/images/careers-masthead-img.svg";
 
 function Careers() {
     const pageTitle = "Careers";
@@ -7,8 +8,31 @@ function Careers() {
         document.title = `${pageTitle} | Frim`;
     }, [pageTitle]);
     return (
-        <section className="careers">
-            
+        <section className="careers-masthead">
+            <div className="careers-masthead-column">
+                <div className="careers-masthead-row">
+                    <div>
+                        <h1>Become part of something great</h1>
+                    </div>
+                    <div>
+                        <p>Frim is on a journey to change the digital era. Explore remote-friendly, flexible opportunities within an organization that wants you to succeed.</p>
+                    </div>
+                </div>
+                <div className="careers-masthead-row">
+                    <div>
+                        <a className="btn-1" href="/careers/#current-openings">
+                            <span>View Careers</span>
+                            <i className="fa-solid fa-angles-right"></i>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/careers/#current-openings" className="internships-anchor">Looking for internships?</a>
+                    </div>
+                </div>
+            </div>
+            <div className="careers-masthead-column">
+                <img src={CareersMastheadImage} alt="A remote worker sits on a bean bag"/>
+            </div>
         </section>
     );
 }
