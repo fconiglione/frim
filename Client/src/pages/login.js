70c46559bee5c1bcf34e33966909c0f1449a1ed9
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-03-24 23:23:15 -0400

    completed login css

--- Client/src/pages/login.js
+++ Client/src/pages/login.js
@@ -11,53 +11,55 @@ function Login() {
     }, [pageTitle]);
     return (
         <section className="login">
-            <div>
-                <div className="login-title">
-                    <div>
-                        <img src={FrimCloudLogo1} alt="Frim Cloud logo"/>
-                    </div>
-                    <div>
-                        <h1>Good to see you again</h1>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className="login-form">
-                    <form method="post">
-                        <fieldset>
-                            <label>
-                                Your email
-                            </label>
-                            <div className="login-input">
-                                <i class="fa-solid fa-user"></i>
-                                <input type="email" placeholder="Email" />
-                            </div>
-                        </fieldset>
-                        <fieldset>
-                            <label>
-                                Your password
-                            </label>
-                            <div className="login-input">
-                                <i class="fa-solid fa-unlock"></i>
-                                <input type="email" placeholder="Email" />
-                            </div>
-                        </fieldset>
-                        <button className="submit-btn">Sign in</button>
-                    </form>
-                    <div className="login-form-nav">
-                        <a href="/signup">Don't have an account?</a>
-                        <a href="/forgot-password">Forgot your password?</a>
+            <div className="login-container">
+                <div>
+                    <div className="login-title">
+                        <div>
+                            <img src={FrimCloudLogo1} alt="Frim Cloud logo"/>
+                        </div>
+                        <div>
+                            <h1>Good to see you again</h1>
+                        </div>
                     </div>
                 </div>
-                <div className="login-products">
-                    <div className="login-product">
-                        <img src={CeasarLogo1} alt="Ceasar logo"/>
+                <div className="login-main">
+                    <div className="login-form">
+                        <form method="post">
+                            <fieldset>
+                                <label>
+                                    Your email
+                                </label>
+                                <div className="login-input">
+                                    <i class="fa-solid fa-user"></i>
+                                    <input type="email" placeholder="e.g. johndoe@example.com" />
+                                </div>
+                            </fieldset>
+                            <fieldset>
+                                <label>
+                                    Your password
+                                </label>
+                                <div className="login-input">
+                                    <i class="fa-solid fa-unlock"></i>
+                                    <input type="password" placeholder="e.g. frimisthebest1234" />
+                                </div>
+                            </fieldset>
+                            <button className="submit-btn">Sign in</button>
+                        </form>
+                        <div className="login-form-nav">
+                            <a href="/signup">Don't have an account?</a>
+                            <a href="/forgot-password">Forgot your password?</a>
+                        </div>
+                    </div>
+                    <div className="login-products">
+                        <a className="login-product" href="/ceasar" target="_blank">
+                            <img src={CeasarLogo1} alt="Ceasar logo"/>
+                        </a>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div className="login-footer">
-                    <p>© {currentYear} Frim, Inc.</p>
+                <div>
+                    <div className="login-footer">
+                        <p>© {currentYear} Frim, Inc.</p>
+                    </div>
                 </div>
             </div>
         </section>
