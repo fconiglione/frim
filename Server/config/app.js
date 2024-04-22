00a7df87adfbe61fec2d58acf83cd24f8db46d7d
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-04-22 14:16:58 -0400

    cleaned up server files

---
+++ Server/config/app.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+
+dotenv.config();
+
+app.use(cors({
+    origin: ['http://localhost:4200', 'http://localhost:3000', 'https://www.frim.io', 'https://www.api.frim.io', 'https://www.cloud.frim.io'],
+    credentials: true
+}))
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+module.exports = app;
