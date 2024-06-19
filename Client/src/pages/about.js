5b5fd84e935926c2ee997e7782752af636e53d57
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-06-18 23:16:11 -0400

    about-story complete

--- Client/src/pages/about.js
+++ Client/src/pages/about.js
@@ -1,5 +1,7 @@
 import { React } from "react";
 import { useEffect } from "react";
+import AboutStoryImg1 from "../assets/images/about-story-img-1.jpg";
+import AboutStoryImg2 from "../assets/images/about-story-img-2.jpg";
 
 function About() {
     const pageTitle = "About";
@@ -36,16 +38,18 @@ function About() {
                     </div>
                 </div>
             </div>
-            {/* <div className="about-story">
-                <div className="about-highlight-column">
+            <div className="about-story">
+                <div className="about-story-column">
                     <h1>Our story</h1>
-                    <p></p>
+                    <p>Now more than ever, people are trying to escape the 9-5 status quo and become true entrepreneurs. This spirit of innovation and independence is the foundation of Frim Technologies. Founded in late 2023 by Francesco Coniglione, Frim was born out of a desire to empower entrepreneurs and small to medium-sized businesses with the tools they need to succeed.</p>
+                    <p>In a world where complexity often hinders progress, we are committed to offering a refreshing alternative. Our vision is clear: <i>to provide young businesses with an affordable, easy-to-use software platform that challenges industry giants.</i> We believe in simplicity over complexity, functionality over feature overload, and user experience at the heart of every line of code. <b>That is what we believe in.</b></p>
+
                 </div>
-                <div className="about-highlight-column">
-                    <img src="/images/about/story.jpg" alt="Our Story" />
-                    <img src="/images/about/story.jpg" alt="Our Story" />
+                <div className="about-story-column">
+                    <img className="about-story-img-1" src={AboutStoryImg1} alt="An aerial view of Toronto, ON" />
+                    <img className="about-story-img-2" src={AboutStoryImg2} alt="Our Story" />
                 </div>
-            </div> */}
+            </div>
         </section>
     );
 }
